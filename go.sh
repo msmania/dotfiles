861d16d72fc291d39e35a5f60bced227dac1055a
Toshihito Kikuchi <k.toshihito@yahoo.de>
2016-05-08 13:10:36 -0700

    Initial check-in

---
+++ go.sh
@@ -0,0 +1,26 @@
+for i in *.conf; do
+    src=$(readlink -f $i)
+    dst=~/.${i%%.*}
+    if [ -f "$dst" ]; then
+        echo "$dst exists. skip."
+    else
+        cmd="ln -s $src $dst"
+        echo $cmd
+        $cmd
+    fi
+done
+
+if [ -d ~/.config/sublime-text-3/Packages/User ]; then
+    for i in sublime-text-3/Packages/User/*.*; do
+        fname=$(printf %q "$i")
+        src=$(readlink -f "$fname")
+        dst=~/.config/$fname
+        if [ -f "$dst" ]; then
+            echo "$dst exists. skip."
+        else
+            cmd="ln -s $src $dst"
+            echo $cmd
+            $cmd
+        fi
+    done
+fi
