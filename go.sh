edbc9ce73a349fda4363bf63a727162ab5d37c46
tokikuch <k.toshihito@yahoo.de>
2018-08-16 20:12:21 -0700

    Remove Atom and Sublime config

--- go.sh
+++ go.sh
@@ -25,33 +25,3 @@ for i in templates/*; do
         $cmd
     fi
 done
-
-if [ -d ~/.config/sublime-text-3/Packages/User ]; then
-    for i in sublime-text-3/Packages/User/*.*; do
-        fname=$(printf %q "$i")
-        src=$(readlink -f "$fname")
-        dst=~/.config/$fname
-        if [ -f "$dst" ]; then
-            echo "$dst exists. skip."
-        else
-            cmd="ln -s $src $dst"
-            echo $cmd
-            $cmd
-        fi
-    done
-fi
-
-if [ -d ~/.atom ]; then
-    for i in atom/*.*; do
-        fname=$(basename "$i")
-        src=$(readlink -f "$i")
-        dst=~/.atom/$fname
-        if [ -f "$dst" ]; then
-            echo "$dst exists. skip."
-        else
-            cmd="ln -s $src $dst"
-            echo $cmd
-            $cmd
-        fi
-    done
-fi
